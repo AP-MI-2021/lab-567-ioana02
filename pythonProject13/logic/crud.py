b58118dcf27a0cd21b2202109db1de57075ecdef
ioana02 <faladauioana@gmail.com>
2021-10-27 11:13:23 +0300

    tema

---
+++ pythonProject13/logic/crud.py
@@ -0,0 +1,67 @@
+from domain.obiect import creeaza_obiect
+
+
+def add(lst_obiecte, id_obiect, nume, descriere, pret, locatie):
+    """
+     Adauga un obiect
+    :param lst_obiecte: lista de obiecte
+    :param id_obiect: id-ul
+    :param nume: numele
+    :param descriere: descrierea
+    :param pret: pretul
+    :param locatie: locatia
+    :return: o lista noua formata din lst_obiecte si noul obiect
+    """
+    obiect = creeaza_obiect(id_obiect, nume, descriere, pret, locatie)
+    return lst_obiecte + [obiect]
+
+
+def update(lst_obiecte, id_obiect, nume, descriere, pret, locatie):
+    """
+    Actualizeaza un obiect
+    :param lst_obiecte: lista de obiecte
+    :param id_obiect: id-ul
+    :param nume: numele
+    :param descriere: descrierea
+    :param pret: pretul
+    :param locatie: locatia
+    :return: o lista cu obiectele actualizate
+    """
+    noua_lst = []
+    for obiect in lst_obiecte:
+        if get_id_obiect(obiect) == id_obiect:
+            obiect_nou = creeaza_obiect(id_obiect, nume, descriere, pret, locatie)
+            noua_lst.append(obiect_nou)
+        else:
+            noua_lst.append(obiect)
+    return noua_lst
+
+
+def delete(lst_obiecte, id_obiect):
+    """
+    Sterge un obiect din lista
+    :praram lst_obiecte: lista de obiecte
+    :param id_obiect: id-ul
+    :return: o lista noua fara obiectul cu id-ul id_obiect
+    """
+    noua_lst = []
+    for obiect in lst_obiecte:
+        if get_id_obiect != id_obiect:
+            noua_lst.append(obiect)
+    return noua_lst
+
+
+def read(lst_obiecte, id_obiect: int = None):
+    """
+    Citeste un obiect din lista
+    :param lst_obiecte: o lista de obiecte
+    :param id_obiect: id-ul
+    :return: obiectul cu id-ul id_obiect sau lista cu toate obiectele, dac id_obiect=None
+    """
+    obiect_id = None
+    for obiect in lst_obiecte:
+        if get_id_obiect(obiect) == id_obiect:
+            obiect_id = obiect
+    if obiect_id:
+        return obiect_id
+    return lst_obiecte
---
+++ pythonProject13/tests/teste.py
@@ -0,0 +1,49 @@
+from domain.obiect import creeaza_obiect, get_id_obiect, get_nume, get_descriere, get_pret, get_locatie
+from logic.crud import read, delete
+
+
+def get_date():
+    return [
+        creeaza_obiect(12, "jucarie", "pentru copii", 26, "sasr"),
+        creeaza_obiect(14445, "masa", "culoare maro", 234, "sura"),
+        creeaza_obiect(23512, "minge", "rosie", 23, "asga"),
+    ]
+
+def test_create():
+    obiecte = get_date()
+    params = (2423, "minge", "verde", 24, "dsfa")
+    c_nou = creeaza_obiect(*params)
+    noi_obiecte = creeaza(obiecte, *params)
+
+    assert c_nou in noi_obiecte
+    assert len(noi_obiecte) == len(obiecte) + 1
+    assert get_id_obiect(read(obiecte, 12)) == 12
+    assert get_nume(read(obiecte, 2)) == "masa"
+    assert get_descriere(read(obiecte, 2)) == "culoare maro"
+    assert get_pret(read(obiecte, 3)) == 23
+    assert get_locatie(read(obiecte, 1)) == "sasr"
+
+def test_read():
+    obiecte = get_date()
+    some_o = obiecte[2]
+    assert read(obiecte, get_id_obiect(some_o)) == some_o
+    assert read(obiecte, None) == obiecte
+
+
+def test_update():
+    obiecte = get_date()
+    o_updated = creeaza_obiect(23512, "minge", "verde", 23, "asga")
+    updated = update(obiecte, 23512, "minge", "verde", 23, "asga" )
+    assert o_updated in updated
+    assert o_updated not in obiecte
+    assert len(obiecte) == len(updated)
+
+def test_delete():
+    obiecte = get_date()
+    to_delete = 1
+    o_deleted = read(obiecet, to_delete)
+    deleted = delete(obiecet, to_delete)
+    assert o_deleted not in deleted
+    assert o_deleted in obiecte
+    assert len(deleted) == len(obiecte) - 1
+
