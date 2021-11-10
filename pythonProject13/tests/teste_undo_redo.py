207c852f6e67029e9be158c5372b88eafbb16b8b
ioana02 <faladauioana@gmail.com>
2021-11-10 13:57:15 +0200

    tema

--- pythonProject13/tests/teste_undo_redo.py
+++ pythonProject13/tests/teste_undo_redo.py
@@ -11,23 +11,23 @@ def test_undo_redo():
     redo_lista.clear()
     lista = adauga_obiect(lista, 1, "masa", "culoare maro", 231, "absn")
 
-    #adaugam inca un obiect
+    #adaugam inca un obiect in lista
     undo_lista.append(lista)
     redo_lista.clear()
     lista = adauga_obiect(lista, 3, "minge", "verde", 23, "asmn")
 
-    #adaugam inca un obiect
+    #adaugam inca un obiect in lista
     undo_lista.append(lista)
     redo_lista.clear()
     lista = adauga_obiect(lista, 4, "minge", "rosie", 24, "asmn")
 
-    #undo scoate ultimul obiect pe care l-am adaugat
+    #undo scoate ultimul obiect pe care l-am adaugat in lista
     if len(undo_lista) > 0:
         redo_lista.append(lista)
         lista = undo_lista.pop()
     assert len(lista) == 2
 
-    #undo scoate penultimul obiect pe care l-am adaugat
+    #undo scoate penultimul obiect pe care l-am adaugat in lista
     if len(undo_lista) > 0:
         redo_lista.append(lista)
         lista = undo_lista.pop()
